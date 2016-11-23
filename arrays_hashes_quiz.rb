def480d9c694889be523b182f0c37bbd89d6e98b
Rory MacDonald <rorymacdonald6@hotmail.com>
2016-11-23 16:11:44 +0000

    second section done

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -31,14 +31,17 @@ lines = ['Gyle Centre', 'Edinburgh Park', 'Murrayfield Stadium', 'Haymarket', 'P
 my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
 
 # 1. How would you return the string `"One"`?
-
+# puts my_hash[1]
 # 2. How would you return the string `"Two"`?
-
+# puts my_hash[:two]
 # 3. How would you return the number `2`?
-
+# puts my_hash["two"]
 # 4. How would you add `{3 => "Three"}` to the hash?
-
+my_hash[3] = "Three"
+puts my_hash
 # 5. How would you add `{:four => 4}` to the hash?
+my_hash[:four] = 4
+puts my_hash
 
 ### C. Given the following data structure:
 
